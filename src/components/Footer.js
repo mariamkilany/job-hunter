30d1c03edb52cdea8cdf9fe9b35b5ed6c7ed938f
yasmin <20191815@fue.edu.eg>
2024-03-05 22:13:20 +0200

    nav, footer,contactus,aboutus

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,51 @@
+import Link from "next/link";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4 shadow-lg">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center mb-4 sm:mb-0  rtl:space-x-reverse"
+          >
+            <img
+              src="/Images/logo.png"
+             width={"100px"}
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Job Hunters
+            </span>
+          </a>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Companies
+              </a>
+            </li>
+            
+            <li>
+              <Link href="/ContactUs" className="hover:underline me-4 md:me-6">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            Job Hunters™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
 }
