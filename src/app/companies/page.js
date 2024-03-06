8b01ca822d9ff920db7d845b02c466360c922d55
mariam-eltayeb <mariam.eltayeb@outlook.com>
2024-03-06 21:50:30 +0200

    landingedits

---
+++ src/app/companies/page.js
@@ -0,0 +1,12 @@
+import CompanyHeader from '@/components/companyPage/companyHeader';
+import React from 'react';
+
+const Compaines = () => {
+    return (
+        <div>
+            <CompanyHeader></CompanyHeader>
+        </div>
+    );
+}
+
+export default Compaines;
--- src/components/landingPage/categories.js
+++ src/components/landingPage/categories.js
@@ -6,36 +6,35 @@ const Categories = () => {
     return (
         <div className='container py-8 mx-auto' >
             <h2 className='text-3xl font-bold  '>
-                Explore our <span className="text-lightGreen"> Compaines</span>
+                Explore our <span className="text-primary"> Compaines</span>
             </h2>
 
             <div className='grid  gap-10  py-8  sm:auto-cols-auto md:grid-cols-5 '>
                 <div>
-                    <img src="/Images/logo/intel.png" style={{width:"100%" , height:"90px"}}>
+                    <img src="/Images/logo/intel.png" className='m-auto' style={{ width:"60%" , height:"40px"}} alt='company logo'>
+                    </img>
+
+                </div>
+                <div>
+                    <img src="/Images/logo/talkit.png"  className='m-auto' style={{width:"60%" , height:"40px"}} alt='company logo' >
 
                     </img>
 
                 </div>
                 <div>
-                    <img src="/Images/logo/facebookLogo.png" style={{width:"100%" , height:"90px"}}>
+                    <img src="/Images/logo/tesla.png" className='m-auto' style={{width:"60%" , height:"40px"}} alt='company logo'>
 
                     </img>
 
                 </div>
                 <div>
-                    <img src="/Images/logo/microsoft.webp" style={{width:"100%" , height:"90px"}}>
+                    <img src="/Images/logo/amnda.png" className='m-auto' style={{width:"60%" , height:"40px"}} alt='company logo'>
 
                     </img>
 
                 </div>
                 <div>
-                    <img src="/Images/logo/tesla.jpg" style={{width:"100%" , height:"90px"}}>
-
-                    </img>
-
-                </div>
-                <div>
-                    <img src="/Images/logo/googleLogo.png" style={{width:"100%" , height:"90px"}}>
+                    <img src="/Images/logo/vodafone.png" className='m-auto' style={{width:"60%" , height:"40px"}} alt='company logo'>
 
                     </img>
 
