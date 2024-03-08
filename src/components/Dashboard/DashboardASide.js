c6872de69e4186aabef26e8590104a56fa0aab56
Youssef Adly <youssefadly404@gmail.com>
2024-03-08 19:02:14 +0200

    fix sidebar

--- src/components/Dashboard/DashboardASide.js
+++ src/components/Dashboard/DashboardASide.js
@@ -4,7 +4,7 @@ const DashboardASide = () => {
   return (
     <aside
       id="sidebar"
-      className="fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 w-64 h-full pt-16 duration-75 lg:flex transition-width"
+      className="fixed top-0 left-0 z-20 flex-col flex-shrink-0 hidden w-64 h-full pt-16 duration-75 lg:flex transition-width"
       aria-label="Sidebar"
     >
       <div className="relative flex flex-col flex-1 min-h-0 pt-0 bg-white border-r border-gray-200">
--- src/components/Dashboard/Profile.js
+++ src/components/Dashboard/Profile.js
@@ -7,69 +7,47 @@ const Profile = () => {
         id="main-content"
         className="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64"
       >
-        <div>
-          <img
-            className="object-cover w-full h-32 lg:h-48"
-            src="https://resources.thomascook.in/images/holidays/sightSeeing/8-big-ben-min.jpg"
-            alt
-          />
-        </div>
-        <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
-          <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
-            <div className="flex">
-              <img
-                className="w-24 h-24 rounded-full ring-4 ring-blue-300 sm:h-32 sm:w-32"
-                src="https://qph.cf2.quoracdn.net/main-thumb-554097988-200-xietklpojlcioqxaqgcyykzfxblvoqrb.jpeg"
-                alt
-              />
-            </div>
-            <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
-              <div className="flex-1 min-w-0 mt-6 sm:hidden md:block">
-                <h1 className="text-2xl font-bold text-blue-300 truncate">
-                  Shehab coding
-                </h1>
-              </div>
-              <div className="flex flex-col mt-6 space-y-3 justify-stretch sm:flex-row sm:space-x-4 sm:space-y-0">
-                <button
-                  type="button"
-                  className="inline-flex justify-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  <svg
-                    className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
-                    <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
-                  </svg>
-                  <span>Message</span>
-                </button>
-                <button
-                  type="button"
-                  className="inline-flex justify-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  <svg
-                    className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  <span>Call</span>
-                </button>
-              </div>
-            </div>
+        <div className="w-3/4 p-10">
+          <div>
+            <img
+              className="object-cover w-full h-32 lg:h-48"
+              src="https://resources.thomascook.in/images/holidays/sightSeeing/8-big-ben-min.jpg"
+              alt
+            />
           </div>
-          <div className="flex-1 hidden min-w-0 mt-6 sm:block md:hidden">
-            <h1 className="text-2xl font-bold text-blue-300 truncate">
-              Shehab coding
-            </h1>
+          <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
+            <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+              <div className="flex">
+                <img
+                  className="w-24 h-24 rounded-full ring-4 ring-blue-300 sm:h-32 sm:w-32"
+                  src="https://qph.cf2.quoracdn.net/main-thumb-554097988-200-xietklpojlcioqxaqgcyykzfxblvoqrb.jpeg"
+                  alt
+                />
+              </div>
+              <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                <div className="flex-1 min-w-0 mt-6 sm:hidden md:block">
+                  <h1 className="text-2xl font-bold text-blue-300 truncate">
+                    Youssef Adly
+                  </h1>
+                  <h6 className="font-light text-gray-500">
+                    Product Designer at <b>Twitter</b>
+                  </h6>
+                </div>
+                <div className="flex flex-col mt-6 space-y-3 justify-stretch sm:flex-row sm:space-x-4 sm:space-y-0">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-5 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                  >
+                    <span>Edit Profile</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="flex-1 hidden min-w-0 mt-6 sm:block md:hidden">
+              <h1 className="text-2xl font-bold text-blue-300 truncate">
+                Youssef Adly
+              </h1>
+            </div>
           </div>
         </div>
       </div>
